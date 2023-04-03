ef633978316f32f13ebade1f15994ea7c091ed8b
aman969 <barsaiya.aman95@gmail.com>
2023-04-03 15:17:06 -0400

    update

--- final_project/static/mywebscript.js
+++ final_project/static/mywebscript.js
@@ -1,6 +1,5 @@
 let translateToFrench = ()=>{
     textToTranslate = document.getElementById("textToTranslate").value;
-
     let xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200) {
